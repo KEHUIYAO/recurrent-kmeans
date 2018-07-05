441e1f951a6ea9d113f458c55785060116edccb7
KEHUIYAO <ykhray19960527@gmail.com>
2018-07-05 20:19:43 +0800

    modify u10 and u12

--- step1/u10.m
+++ step1/u10.m
@@ -7,7 +7,7 @@ parfor i=1:40
 rng('shuffle')%set up the random seed
 u_v=[100 200 300];% two possible values of change-points
 K_d=3;% two clusters
-m=40; % # of drivers
+m=80; % # of drivers
 l1=0.25;% intensity rate before the change-point
 l2=0.1; % intensity rate after the change-point
 [z,Nj,C,tau_list(i,:)]=latent_simu_f(u_v,m,l1,l2,K_d);%___________data simulation end_______________
